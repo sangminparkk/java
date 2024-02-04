3e592c2887a52983432d8daa8a12b954d137b48b
chandler <parksm0529@gmail.com>
2024-02-04 17:09:42 +0900

    변수 명명 규칙 (낙타표기법)
    - 클래스: 첫글자 대문자로 시작
    - 그외: 첫글자 소문자로 시작

---
+++ src/variable/Var9.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var9 {
+    public static void main(String[] args) {
+
+        /**
+         * 변수 이름
+         * 1. 클래스 : 첫글자 대문자로 시작 + 낙타표기법
+         * 2. 클래스를 제외한 나머지 : 첫글자 소문자 시작 + 낙타표기법
+         *
+         * 예외
+         * 1) 패키지 : All 소문자
+         * 2) 상수 : All 대문자 + 언더바 조합
+         */
+
+        // 변수명은 명확하게
+        int studentAccount;
+        int userAccount;
+        int orderAccount;
+
+        int maxScore;
+
+    }
+}
