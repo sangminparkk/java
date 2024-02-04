6fe8e965e725893390f6610283ed12cd8a818dc4
chandler <parksm0529@gmail.com>
2024-02-04 13:53:03 +0900

    변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+
+        int a; // 변수 선언
+        a = 10; // 변수 초기화 (값 할당)
+
+        System.out.println("a = " + a);
+        System.out.println("====변수값 변경===");
+        a = 50; // 값 변경
+        System.out.println("a = " + a);
+    }
+}
