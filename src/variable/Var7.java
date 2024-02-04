4186b39a247768eb7fdb83abbdc61e3ff75657de
chandler <parksm0529@gmail.com>
2024-02-04 15:31:53 +0900

    변수 타입
    *리터럴: 개발자가 직접 적은, 고정된 값을 프로그래밍에서는 리터럴이라고 표현합니다.
    
    예) 변수에 리터럴을 담아서 쓴다.

---
+++ src/variable/Var7.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+
+        /**
+         * 변수 타입
+         * *리터럴 : 개발자가 직접 적은, 고정된 값을 리터럴이라고 표현합니다.
+         * 그래서 변수에 리터럴을 담아서 쓴다!
+         */
+        int a = 100; // 정수 리터럴
+        double b = 10.55555555555; // 실수 리터럴
+        boolean c = true; // 불리언 리터럴, true or false (판단)
+
+        char d = 'A'; // 문자 리터럴 - 문자 하나 > Too many characters in character literal
+        String e = "Hello Java"; // 문자열 리터럴 - 문자열을 다루기 위한 '특별한' 타입
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        System.out.println("===Long 클래스===");
+        Long number = 10L; // 클래스
+        System.out.println(number);
+    }
+}
