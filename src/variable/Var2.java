426130a3091e8c8dae6903aab1dea1fdf2f1ff0b
chandler <parksm0529@gmail.com>
2024-02-04 13:49:47 +0900

    변수 시작

---
+++ src/variable/Var2.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        /**
+         * 변수 설정
+         * 1. 변수 선언
+         * 2. 변수 초기화 (= 값 할당) > may not have been initialized
+         */
+        int number;
+        number = 10;
+
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+    }
+
+}
